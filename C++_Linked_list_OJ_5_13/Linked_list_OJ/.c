28a68aafec862c7ba0917f9a66eb604470ed6a5c
chenkaixuanQQ <3038966841@qq.com>
2024-05-14 00:30:12 +0800

    Went through all the data structures

---
+++ C++_Linked_list_OJ_5_13/Linked_list_OJ/.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Linked list in C++.
+//struct ListNode {
+//    int val;        // 节点值
+//    ListNode* next; // 后继节点引用
+//    ListNode(int x) : val(x), next(NULL) {}
+//};
+
